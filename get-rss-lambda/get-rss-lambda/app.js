af2346f53ad8b0910cf144b864d7f7f9638b48ca
kuisiba <kuisiba@gmail.com>
2020-01-01 18:41:38 +0900

    2

--- get-rss-lambda/get-rss-lambda/app.js
+++ get-rss-lambda/get-rss-lambda/app.js
@@ -1,7 +1,9 @@
 const aws = require('aws-sdk');
 const Parser = require('rss-parser');
 const parser = new Parser();
-const s3 = new aws.S3();
+const s3 = new aws.S3({
+  region: 'ap-northeast-1',
+});
 const moment = require('moment-timezone');
 
 
@@ -10,17 +12,26 @@ const testUrl = 'https://note.com/gravestone11/rss';
 const GenS3Params = (body) => {
   const currentTime = moment().toISOString();
   return {
-    Bucket: "NoteRssDataBucket",
+    Bucket: "noterssdatabucket",
     Key: "data/" + currentTime + ".json",
     Body: JSON.stringify(body),
+    ContentType: 'application/json',
   }
 }
 exports.lambdaHandler = async (event, context) => {
     try {
-      const feed = await parser.parseURL(testUrl);
-      const s3Params = GenS3Params(feed);
+      // const feed = await parser.parseURL(testUrl);
+      const s3Params = GenS3Params({"test": "test3"});
       // console.log(s3Params);
-      await s3.putObject(s3Params);
+      s3.putObject(s3Params, function(err, data) {
+        if (err) {
+          console.log("====error===");
+          console.log(err, err.stack);
+        } else {
+          console.log("====success===");
+          console.log(data);
+        }
+      });
     } catch (err) {
         console.log(err);
         return err;
